050a870616d7bde8fd32d03125cdbe849115ac5f
Sujal Joshi <work.sujaljoshi@gmail.com>
2025-04-11 20:29:22 +0530

    Fetched address in order details

--- backend/order/views.py
+++ backend/order/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from .models import Order, OrderItems, Event
+from .models import Order, OrderItems
+from events.models import Event
 from .serializers import OrderSerializer, OrderItemsSerializer, ProductSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.db import transaction
