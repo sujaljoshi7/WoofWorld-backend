eb6cd4fc1628d76ee64b0edc80f8082ff6e427b0
Sujal Joshi <work.sujaljoshi@gmail.com>
2025-04-18 16:37:52 +0530

    Fixed orders api response

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("<int:event_id>/", get_specific_event_data, name="get_specific_event_data"),
     path("category/<int:category_id>/activate/", activate_category, name="activate_category"),
     path("category/<int:category_id>/deactivate/", deactivate_category, name="deactivate_category"),
-    path("past-event-images/", PastEventImageView, name="PastEventImageView"),
+    path("past-event-images/", PastEventImageView.as_view(), name="PastEventImageView"),
 ]
